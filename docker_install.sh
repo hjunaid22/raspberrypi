20c1772f8258bd980b2bbabeea646927f8bcba2d
Junaid Hassan <57299484+hjunaid22@users.noreply.github.com>
2023-10-28 12:42:23 -0400

    Create docker_install.sh

---
+++ docker_install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+apt-get update
+apt-get install ca-certificates curl gnupg -y
+apt-get install vim -y
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/raspbian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg -y
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/raspbian \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+apt-get upgrade
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+docker ps
