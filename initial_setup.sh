071cbc3e528367f6c9bbcb45afa9f7e9e6ca3151
Junaid Hassan <57299484+hjunaid22@users.noreply.github.com>
2023-10-28 14:41:12 -0400

    Update initial_setup.sh

---
+++ initial_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "alias='ls -al'" >> /etc/profile
+######## installing cifscloak:- cifs share encryption ###########
+#sudo apt install python3-pip -y
+#sudo pip3 install git+https://github.com/sudoofus/cifscloak.git --break-system-packages
+#sudo python3 -m pip install cifscloak
+
+######
+touch /home/h.junaid/.credentials
+chmod 600
+sudo mkdir /backup
+sudo echo "//192.168.1.5/Open_Share /backup cifs credentials=/home/h.junaid/.credentials,rw,uid=1001,gid=1001" >> /etc/fstab
+sudo groupadd admin
+sudo usermod -aG admin h.junaid
