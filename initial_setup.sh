810b191a5001a7d0b5a0df14b661084a82dbbe2d
Junaid Hassan <57299484+hjunaid22@users.noreply.github.com>
2024-01-15 07:41:27 -0500

    Update initial_setup.sh
    adding apache int eh installing list

--- initial_setup.sh
+++ initial_setup.sh
@@ -5,7 +5,7 @@ read "hostname"
 echo "alias='ls -al'" >> /etc/profile
 ######## installing Packages ###########
 sudo apt-get install dnsutils #nslookup and dig
-
+sudo apt-get install apache2
 ######
 touch /home/h.junaid/.credentials
 echo "username=download" >> /home/h.junaid/.credentials
@@ -15,3 +15,4 @@ sudo echo "//192.168.1.5/Open_Share /backup cifs credentials=/home/h.junaid/.cre
 sudo groupadd admin
 sudo usermod -aG admin h.junaid
 ######## Adding hostname ####
+######## 
