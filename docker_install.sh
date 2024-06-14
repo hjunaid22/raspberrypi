0cb273b0de45efc5efa32f40af8b100ab8bafe5b
Junaid Hassan <57299484+hjunaid22@users.noreply.github.com>
2024-06-14 15:18:39 -0400

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -3,13 +3,16 @@ apt-get update
 apt-get install ca-certificates curl gnupg -y
 apt-get install vim -y
 install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/raspbian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg -y
+curl -fsSL https://download.docker.com/linux/raspbian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/raspbian \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-apt-get upgrade
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get update -y
+apt-get upgrade -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 docker ps
+if echo $?=127 than 
+curl -sSL https://get.docker.com | sh
+fi
